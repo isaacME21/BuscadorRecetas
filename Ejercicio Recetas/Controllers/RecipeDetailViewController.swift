f6bcb1c9f5bf8125e0bbc03b24e85458d2f2ba43
Isaac Maya <isaac.maya@dwit.mx>
2021-03-21 17:58:41 -0600

    Punto 4 finalizado: -Se modifico el modelo de respuesta para obtener los ingredientes. -Se modifico el view model para obtener un array de los ingredientes -Se agregaron métodos para llenar la tabla de ingredientes.

--- Ejercicio Recetas/Controllers/RecipeDetailViewController.swift
+++ Ejercicio Recetas/Controllers/RecipeDetailViewController.swift
@@ -17,7 +17,23 @@ class RecipeDetailViewController: UIViewController {
         self.title = recipeVM.strMeal
         detailTextView.text = recipeVM.strInstructions
     }
+}
 
-   
-
+extension RecipeDetailViewController : UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return recipeVM.getIngredients().count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "IngredientesCell")!
+        let ingredient = recipeVM.getIngredients()[indexPath.row]
+        cell.textLabel?.text = "Ingrediente"
+        cell.detailTextLabel?.text = ingredient
+        
+        return cell
+    }
 }
--- Ejercicio Recetas/View Models/RecipeViewModel.swift
+++ Ejercicio Recetas/View Models/RecipeViewModel.swift
@@ -47,6 +47,74 @@ struct  RecipeViewModel{
         return self.recipe.strInstructions
     }
     
+    func getIngredients() -> [String]{
+        var ingredientsTemp : [String] = []
+        if self.recipe.strIngredient1 != nil{
+            ingredientsTemp.append(self.recipe.strIngredient1!)
+        }
+        if self.recipe.strIngredient2 != nil{
+            ingredientsTemp.append(self.recipe.strIngredient2!)
+        }
+        if self.recipe.strIngredient3 != nil{
+            ingredientsTemp.append(self.recipe.strIngredient3!)
+        }
+        if self.recipe.strIngredient4 != nil{
+            ingredientsTemp.append(self.recipe.strIngredient4!)
+        }
+        if self.recipe.strIngredient5 != nil{
+            ingredientsTemp.append(self.recipe.strIngredient5!)
+        }
+        if self.recipe.strIngredient6 != nil{
+            ingredientsTemp.append(self.recipe.strIngredient6!)
+        }
+        if self.recipe.strIngredient7 != nil{
+            ingredientsTemp.append(self.recipe.strIngredient7!)
+        }
+        if self.recipe.strIngredient8 != nil{
+            ingredientsTemp.append(self.recipe.strIngredient8!)
+        }
+        if self.recipe.strIngredient9 != nil{
+            ingredientsTemp.append(self.recipe.strIngredient9!)
+        }
+        if self.recipe.strIngredient10 != nil{
+            ingredientsTemp.append(self.recipe.strIngredient10!)
+        }
+        if self.recipe.strIngredient11 != nil{
+            ingredientsTemp.append(self.recipe.strIngredient11!)
+        }
+        if self.recipe.strIngredient12 != nil{
+            ingredientsTemp.append(self.recipe.strIngredient12!)
+        }
+        if self.recipe.strIngredient13 != nil{
+            ingredientsTemp.append(self.recipe.strIngredient13!)
+        }
+        if self.recipe.strIngredient14 != nil{
+            ingredientsTemp.append(self.recipe.strIngredient14!)
+        }
+        if self.recipe.strIngredient15 != nil{
+            ingredientsTemp.append(self.recipe.strIngredient15!)
+        }
+        if self.recipe.strIngredient16 != nil{
+            ingredientsTemp.append(self.recipe.strIngredient16!)
+        }
+        if self.recipe.strIngredient17 != nil{
+            ingredientsTemp.append(self.recipe.strIngredient17!)
+        }
+        if self.recipe.strIngredient18 != nil{
+            ingredientsTemp.append(self.recipe.strIngredient18!)
+        }
+        if self.recipe.strIngredient19 != nil{
+            ingredientsTemp.append(self.recipe.strIngredient19!)
+        }
+        if self.recipe.strIngredient20 != nil{
+            ingredientsTemp.append(self.recipe.strIngredient10!)
+        }
+        
+        return ingredientsTemp
+    }
+    
+    
+    
     init(_ recipe : Recipe) {
         self.recipe = recipe
     }
